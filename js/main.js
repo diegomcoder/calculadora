10964be5c4938cb311a9a0f68e235c497cfe0789
diegomdsv <mdsidiego@gmail.com>
2023-07-22 22:24:54 -0300

    Update js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,53 @@
+const display = document.querySelector('.display-digits')
 const validOperators = ['+','-','*','/']
 
 function isValidNumber(command) {
-    return parseInt(command) >= 0 && parseInt(command) <= 9
+    const isANumber = parseInt(command) >= 0 && parseInt(command) <= 9
+    const displayIsClear = display.innerText == '0'
+
+    if (isANumber && displayIsClear) {
+        display.innerText = command
+    } else if (isANumber) {
+        display.innerText = display.innerText + command
+    }
 }
 
 function isValidOperator(command) {
-    return validOperators.some((item)=> item == command)
+    if (validOperators.some((item)=> item == command)) {
+        display.innerText = display.innerText + command
+    }
+}
+
+function backspace(command) {
+    const displayLenght = display.innerText.length
+
+    if (command == 'Backspace' && displayLenght > 1) {
+        display.innerText = display.innerText.slice(0, displayLenght -1)
+    } else if (command == 'Backspace' && display.innerText != '0') {
+        display.innerText = '0'
+    }
+}
+
+function clear(command) {
+    if (command == 'c')
+        display.innerText = 0
 }
 
 
 window.addEventListener('keyup', function (e) {
     const keyPressed = e.key
 
-    if (isValidOperator(keyPressed))
-        console.log('The command is an operator');
-    else if (isValidNumber(keyPressed))
-        console.log('The command is a number')
+    isValidOperator(keyPressed)
+    isValidNumber(keyPressed)
+    backspace(keyPressed)
 })
 
 window.addEventListener('click', function (e) {
     const elementClicked = e.target
     const keyPressed = elementClicked.dataset.command
 
-    if (isValidOperator(keyPressed))
-        console.log('The command is an operator');
-    else if (isValidNumber(keyPressed))
-        console.log('The command is a number')
+    isValidOperator(keyPressed)
+    isValidNumber(keyPressed)
+    backspace(keyPressed)
+    clear(keyPressed)
 })
